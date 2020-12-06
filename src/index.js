15535aa1d6694cc819c3d3f2bd6324012924bb35
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-05 21:22:01 -0500

    vaiable insert javascrip in html in javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+let luckyNumber = 7;
 ReactDOM.render(
   <div>
     <h1>Hello World</h1>
     <p>Please stop drop and roll.</p>
+    <p>Your lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
 );
