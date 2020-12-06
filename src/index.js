5120b41487804bd3ec09fde2cb2e5684f404a496
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-05 21:54:11 -0500

    lorem picsum

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 let luckyNumber = 7;
 const today = new Date();
 const year = today.getFullYear();
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
-    <h1>Hey World</h1>
+    <h1 className="head"> Hey World</h1>
     <p>Please stop drop and roll.</p>
+    <img alt="random" src={img + "?grayscale"} />
     <p>Your lucky number is {luckyNumber}</p>
     <p>Copyright {year}.</p>
   </div>,
