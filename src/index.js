13a4d5f57220cdfcd0a145c93b8f259cb0190118
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-05 21:32:57 -0500

    copyright year

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 let luckyNumber = 7;
+const today = new Date();
+const year = today.getFullYear();
+
 ReactDOM.render(
   <div>
-    <h1>Hello World</h1>
+    <h1>Hey World</h1>
     <p>Please stop drop and roll.</p>
     <p>Your lucky number is {luckyNumber}</p>
+    <p>Copyright {year}.</p>
   </div>,
   document.getElementById("root")
 );
