dadd023fd2bd17eb11fd81cf9810972961b2c36d
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-06 16:57:26 -0500

    Dynamic greeting and Math random lucky number

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-let luckyNumber = 7;
+let luckyNumber = Math.floor(Math.random() * 10);
 const today = new Date();
 const year = today.getFullYear();
 const img = "https://picsum.photos/200";
 
-const customStyle = {
+const customStyle2 = {
   color: "blue"
 };
+const date = new Date();
+const currentTime = date.getHours();
 
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 ReactDOM.render(
   <div>
-    <h1 className="head"> Hey World</h1>
-    <p style={customStyle}>Please stop drop and roll.</p>
+    <h1 className="heading" style={customStyle}>
+      {greeting}
+    </h1>
+    <h2 className="head"> Hey World</h2>
+    <p style={customStyle2}>Please stop drop and roll.</p>
     <img alt="random" src={img + "?grayscale"} />
     <p>Your lucky number is {luckyNumber}</p>
     <p>Copyright {year}.</p>
