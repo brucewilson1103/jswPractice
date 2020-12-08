a74905ea315ab9c5e5a72e00d64b07485f6f712e
Bruce Wilson <brucewilson1103@gmail.com>
2020-12-07 20:21:32 -0500

    App component folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./components/heading";
+import Lucky from "./components/luckyNumber";
+import Image from "./components/image";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Image />
+      <Lucky />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./heading";
-import Lucky from "./luckyNumber";
-import Image from "./image";
+
 import App from "./App";
 
 ReactDOM.render(
   <div>
-    <Heading />
-    <Image />
-    <Lucky />
+    <App />
   </div>,
   document.getElementById("root")
 );
